0e2c8552615a489a4d8fe0b74fb8cb385d0bc987
mustachedbirdayane <mustachedbirdayane@gmail.com>
2026-02-05 16:05:59 +0900

    シードで初期ユーザーのデータを作成 2/5

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
@@ -11,7 +12,7 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        user::create([
+        User::create([
          'name' => 'ユーザー１',
          'email' => 'user1@email.com',
          'password' => Hash::make('password1'),
