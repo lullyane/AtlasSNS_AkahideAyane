e50dc0fc15db643da7a25e92a8730b17b1671f81
mustachedbirdayane <mustachedbirdayane@gmail.com>
2026-03-04 16:18:17 +0900

    プロフィール編集項目の入力フォームを設置

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -1,7 +1,37 @@
 @extends('layouts.login')
 
 @section('content')
-<h1>プロフィール編集画面</h1>
-
-
+<form action="" >
+  <div class="edit_profile">
+  <div class="user_icon">
+    <img src="images/icon1.png">
+  </div>
+  <div class="profile_item">
+    <div class="row">
+    <p class="item">ユーザー名</p>
+    <p class="form"><input type="text"></p>
+    </div>
+    <div class="row">
+    <p class="item">メールアドレス</p>
+    <p><input type="text"></p>
+    </div>
+    <div class="row">
+    <p class="item">パスワード</p>
+    <p><input type="text"></p>
+    </div>
+    <div class="row">
+    <p class="item">パスワード確認</p>
+    <p><input type="text"></p>
+    </div>
+    <div class="row">
+    <p class="item">自己紹介</p>
+    <p><input type="text"></p>
+    </div>
+    <div class="row">
+    <p class="item">アイコン画像</p>
+    <p><input type="text"></p>
+    </div>
+    </div>
+  </div>
+</form>
 @endsection
