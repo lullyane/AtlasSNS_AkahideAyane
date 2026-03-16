4a6c241a24856219b31b6d92da9b3a2417696eed
mustachedbirdayane <mustachedbirdayane@gmail.com>
2026-03-16 10:02:42 +0900

    修正 メールアドレスのname属性を統一する（passwordも併せて修正）

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,16 +4,24 @@
 
 {!! Form::open(['url' => '/login', 'method' => 'post']) !!}
 @csrf
+
 <div class="form_wrapper">
     <div>
         <p class="form_font form_login">AtlasSNSへようこそ</p>
     </div>
+    @if ($errors->any())
+    <div class="form_font">
+            @foreach ($errors->all() as $error)
+                <p>{{ $error }}</p>
+            @endforeach
+    </div>
+    @endif
     <div class="form_font form_input">
-        {{ Form::label('メールアドレス') }}
+        {{ Form::label('mail','メールアドレス') }}
         {{ Form::text('mail',null,['class' => 'input']) }}
     </div>
     <div class="form_font form_input">
-        {{ Form::label('パスワード') }}
+        {{ Form::label('password','パスワード') }}
         {{ Form::password('password',['class' => 'input']) }}
     </div>
     {{ Form::submit('ログイン') }}
