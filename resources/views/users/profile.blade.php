88be49b2ec809caffa5585ac59244d7811031db8
mustachedbirdayane <mustachedbirdayane@gmail.com>
2026-03-17 11:13:59 +0900

    修正 アイコン・各項目・入力欄・更新ボタンを中央寄せに配置する

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -8,47 +8,51 @@
 <p class="profile_error">{{ $error }}</p>
 @endforeach
 @endif
-<div class="edit_profile">
-    <div class="user_icon">
-        @if(Auth::check())
-        <img src="{{ asset('storage/' . Auth::user()->images) }}" alt="プロフィール画像">
-        @endif
-    </div>
-    {!! Form::open(['url' => '/profile','method' => 'PUT', 'files' => true]) !!}
-    @csrf
-    <div class="profile_item">
-        <div class="row">
-            <!-- ↓for属性(省略不可) ↓ラベルに表示される文字 -->
-            {{ Form::label('username','ユーザー名',['class' => 'item']) }}
-            <!-- ↓name属性(省略不可) ↓初期値 -->
-            {{ Form::text('username',old('username',$user->username),['class' => 'input']) }}
+<!-- edit_wrapperの中央揃え -->
+<div class="edit_profile_wrapper">
+    <!-- 子要素の横並びと間隔調整 -->
+    <div class="edit_profile">
+        <div class="user_icon">
+            @if(Auth::check())
+            <img src="{{ asset('storage/' . Auth::user()->images) }}" alt="プロフィール画像">
+            @endif
         </div>
-        <div class="row">
-            {{ Form::label('mail','メールアドレス',['class' => 'item']) }}
-            {{ Form::text('mail',old('mail',$user->mail),['class' => 'input']) }}
-        </div>
-        <div class="row">
-            {{ Form::label('password','パスワード',['class' => 'item']) }}
-            {{ Form::password('password',['class' => 'input']) }}
-        </div>
-        <div class="row">
-            {{ Form::label('password_confirmation','パスワード確認',['class' => 'item']) }}
-            {{ Form::password('password_confirmation',['class' => 'input']) }}
-        </div>
-        <div class="row">
-            {{ Form::label('bio','自己紹介',['class' => 'item']) }}
-            {{ Form::textarea('bio',old('bio',$user->bio),['class' => 'input']) }}
-        </div>
-        <div class="row file">
-            {{ Form::label('images','アイコン画像',['class' => 'item']) }}
-            <div class="file_wrapper">
-                <label for="images" class="file_label">ファイルを選択</label>
-                {{ Form::file('images', ['id' => 'images', 'class' => 'file_input']) }}
+        {!! Form::open(['url' => '/profile','method' => 'PUT', 'files' => true]) !!}
+        @csrf
+        <div class="profile_item">
+            <div class="row">
+                <!-- ↓for属性(省略不可) ↓ラベルに表示される文字 -->
+                {{ Form::label('username','ユーザー名',['class' => 'item']) }}
+                <!-- ↓name属性(省略不可) ↓初期値 -->
+                {{ Form::text('username',old('username',$user->username),['class' => 'input']) }}
             </div>
+            <div class="row">
+                {{ Form::label('mail','メールアドレス',['class' => 'item']) }}
+                {{ Form::text('mail',old('mail',$user->mail),['class' => 'input']) }}
+            </div>
+            <div class="row">
+                {{ Form::label('password','パスワード',['class' => 'item']) }}
+                {{ Form::password('password',['class' => 'input']) }}
+            </div>
+            <div class="row">
+                {{ Form::label('password_confirmation','パスワード確認',['class' => 'item']) }}
+                {{ Form::password('password_confirmation',['class' => 'input']) }}
+            </div>
+            <div class="row">
+                {{ Form::label('bio','自己紹介',['class' => 'item']) }}
+                {{ Form::textarea('bio',old('bio',$user->bio),['class' => 'input']) }}
+            </div>
+            <div class="row file">
+                {{ Form::label('images','アイコン画像',['class' => 'item']) }}
+                <div class="file_wrapper">
+                    <label for="images" class="file_label">ファイルを選択</label>
+                    {{ Form::file('images', ['id' => 'images', 'class' => 'file_input']) }}
+                </div>
+            </div>
+            <button type="submit">更新</button>
         </div>
-        <button type="submit">更新</button>
+        {!! Form::close() !!}
     </div>
-    {!! Form::close() !!}
 </div>
 
 @endsection
