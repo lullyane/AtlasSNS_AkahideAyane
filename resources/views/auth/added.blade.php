96e13b91efc0ba8e28cb43d7c5271ab283da4d54
mustachedbirdayane <mustachedbirdayane@gmail.com>
2026-03-23 10:20:05 +0900

    修正 「ユーザー登録が完了いたしました。」と「早速ログインをしてみましょう！」を中央揃えに配置したまま、文頭の開始位置を揃える

--- resources/views/auth/added.blade.php
+++ resources/views/auth/added.blade.php
@@ -6,9 +6,11 @@
         <p>{{ session("username") }} さん</p>
         <p>ようこそ！AtlasSNSへ</p>
     </div>
-    <div class="added_text ">
-        <p>ユーザー登録が完了いたしました。</p>
-        <p>早速ログインをしてみましょう！</p>
+    <div class="added_text">
+        <div class="text_left">
+            <p>ユーザー登録が完了いたしました。</p>
+            <p>早速ログインをしてみましょう！</p>
+        </div>
     </div>
 <p><a href="/login" class="added_button">ログイン画面へ</a></p>
 
